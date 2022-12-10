e8eaa38ce9e300997a9b165b2d86a465254415cc
shubham44-code <shubham162001@gmail.com>
2022-12-10 22:58:21 +0530

    Design Updated

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,10 +4,12 @@ import About from "../components/About/About";
 import Skills from "../components/Skills/Skills";
 import Projects from "../components/Projects/Projects";
 import Footer from "../components/Footer/Footer";
+import Gradient from "../components/Gradient/Gradient";
 
 const HomePage = () => {
   return (
     <>
+      <Gradient />
       <Header />
       <Main />
       <About />
